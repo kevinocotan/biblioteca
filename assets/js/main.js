bee610843f2bca265a995b49fae85a482f758974
Kevin Alexander Ocotán <kaocotan@gmail.com>
2024-10-23 17:58:32 -0600

    cambios para adaptar el swiper, no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,83 @@
 /*=============== login ===============*/
 
-const searchButton = document.getElementById('search-button'),
-      searchClose = document.getElementById('search-close'),
-      searchContent = document.getElementById('search-content')
+const searchButton = document.getElementById("search-button"),
+  searchClose = document.getElementById("search-close"),
+  searchContent = document.getElementById("search-content");
 
-/* SEARCH SHOW */     
+/* SEARCH SHOW */
 
-if(searchButton){
-    searchButton.addEventListener('click', () =>{
-        searchContent.classList.add('show-search')
-    })
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
 }
 
 /* SEARCH HIDDEN */
 
-if(searchClose){
-    searchClose.addEventListener('click', () =>{
-        searchContent.classList.remove('show-search')
-    })
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
 }
 
 /*=============== LOGIN ===============*/
 
-const loginButton = document.getElementById('login-button'),
-      loginClose = document.getElementById('login-close'),
-      loginContent = document.getElementById('login-content')
+const loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close"),
+  loginContent = document.getElementById("login-content");
 
-/* LOGIN SHOW */     
+/* LOGIN SHOW */
 
-if(loginButton){
-    loginButton.addEventListener('click', () =>{
-        loginContent.classList.add('show-login')
-    })
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
 }
 
 /* LOGIN HIDDEN */
 
-if(loginClose){
-    loginClose.addEventListener('click', () =>{
-        loginContent.classList.remove('show-login')
-    })
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
 }
 
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
 /*=============== HOME SWIPER ===============*/
 
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerview: 'auto',
+    centeredSlides: 'auto',
+  
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  
+  
+    breakpoints: {
+      1220: {
+          spaceBetween: -32,
+      }
+    }
+  });
+  
 
 /*=============== FEATURED SWIPER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
-
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
