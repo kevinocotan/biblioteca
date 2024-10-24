7264e3c1148e7b165d0dec40d8bb07ffb7239990
Kevin Alexander Ocotán <kaocotan@gmail.com>
2024-10-24 16:57:34 -0600

    home swiper solucionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,25 +47,13 @@ if (loginClose) {
 
 /*=============== HOME SWIPER ===============*/
 
-let swiperHome = new Swiper('.home__swiper', {
-    loop: true,
-    spaceBetween: -24,
-    grabCursor: true,
-    slidesPerview: 'auto',
-    centeredSlides: 'auto',
-  
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  
-  
-    breakpoints: {
-      1220: {
-          spaceBetween: -32,
-      }
-    }
-  });
+let swiperHome = new Swiper ('.home__swiper',{
+  loop: true,
+  spaceBetween: -24,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+})
   
 
 /*=============== FEATURED SWIPER ===============*/
