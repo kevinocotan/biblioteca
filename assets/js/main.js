4d967ec7740f8090463b20e71b4a2f3533cc5704
Kevin Alexander Ocotán <kaocotan@gmail.com>
2024-11-26 15:13:48 -0600

    testimonial terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,21 @@ let swiperNew = new Swiper ('.new__swiper',{
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
+let swiperTestimonial = new Swiper ('.testimonial__swiper',{
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+
+  breakpoints:{
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    }
+  }
+})
+
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
